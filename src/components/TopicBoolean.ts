80403fca26fe9c67bf18cb5bf3dd9d026691b2b1
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-01 11:45:03 +0100

    boolean is finished

--- src/components/TopicBoolean.ts
+++ src/components/TopicBoolean.ts
@@ -2,10 +2,13 @@ import employees from '../data/employees.json';
 
 export const TopicBoolean = () => {
 
-	const employee = employees[0];
+	let html = '';
+	for (const employee of employees) {
+		html += `<p>${employee.firstName} is ${employee.isMarried ? 'married' : 'single'} and ${employee.gender === 'female' ? 'she' : 'he'} ${employee.hasChildren ? 'has children' : "doesn't have children"}.</p>`;
+	}
 
 	return /*html*/ `
 	<h2>Boolean</h2>
-	<p>${employee.firstName} is ${employee.isMarried ? 'married' : 'single'} and ${employee.gender === 'female' ? 'she' : 'he'} ${employee.hasChildren ? 'has children' : "doesn't have children"}.</p>	
+	${html}
 	`
 }
