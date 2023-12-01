a69509f82b331baca0392f57eaefcc26774142e0
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-01 11:36:47 +0100

    before delete

--- src/components/TopicBoolean.ts
+++ src/components/TopicBoolean.ts
@@ -1,10 +1,11 @@
+import employees from '../data/employees.json';
+
 export const TopicBoolean = () => {
 
-	const firstName = 'Maria';
-	const isMarried = false;
+	const employee = employees[0];
 
 	return /*html*/ `
 	<h2>Boolean</h2>
-	<p>${firstName} is ${isMarried ? 'married' : 'single'}.</p>	
+	<p>${firstName} is ${isMarried ? 'married' : 'single'} and ${gender !== 'female' ? 'she' : 'he'} ${hasChildren ? 'has children' : "doesn't have children"}.</p>	
 	`
 }
