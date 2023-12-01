44151192bc1c7f1446b8d1c0e11514d24f48cb94
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-01 11:08:28 +0100

    strings/numbers finished

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import './style.css'
 
+const firstName = 'Georg';
+const age = 44;
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <h1>JavaScript Learn Site</h1>
+<p>${firstName} is ${age}.</p>
+<p>Next year, ${firstName} will be ${age+1}.</p>
+<p>The variable firstName is of type ${typeof firstName}.</p>
+<p>The variable age is of type ${typeof age}.</p>
 `
 
